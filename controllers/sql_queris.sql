f755aed9b194b791309e075b4f78dcdf7476e168
daadbina <amir.daadbin.95@gmail.com>
2024-11-10 15:09:59 +0330

    update sql queris

--- controllers/sql_queris.sql
+++ controllers/sql_queris.sql
@@ -25,11 +25,18 @@ GROUP BY product_name, ui_change, desc_change;
 SELECT 
     product_name,
     AVG(amount) AS avg_sales,
-    STDDEV(amount) AS std_sales,
+    SQRT(SUM((amount - avg_amount) * (amount - avg_amount)) / COUNT(amount)) AS std_sales,
     MIN(amount) AS min_sales,
     MAX(amount) AS max_sales
-FROM invoices
+FROM (
+    SELECT 
+        product_name, 
+        amount,
+        AVG(amount) OVER (PARTITION BY product_name) AS avg_amount
+    FROM invoices
+) AS subquery
 GROUP BY product_name;
+
 
 -- Query name: ui_description_sales_summary
 SELECT 
@@ -40,3 +47,31 @@ FROM test_analysis
 JOIN invoices ON test_analysis.userid = invoices.userid
 GROUP BY ui_change, desc_change;
 
+-- Query name: final_data_query
+SELECT 
+    p.event_name, 
+    i.amount, 
+    t.ui_change, 
+    t.desc_change
+FROM invoices i
+JOIN products p ON i.event_id = p.event_id  -- Join with the tbl_products to get event_name
+JOIN test_analysis t ON i.userid = t.userid
+
+
+-- Query name: product_sales_by_group
+SELECT 
+    CASE 
+        WHEN COALESCE(ui_change, 'no') = 'no' AND COALESCE(desc_change, 'no') = 'no' THEN 'A'
+        WHEN COALESCE(ui_change, 'no') = 'yes' AND COALESCE(desc_change, 'no') = 'no' THEN 'B'
+        WHEN COALESCE(ui_change, 'no') = 'no' AND COALESCE(desc_change, 'yes') = 'yes' THEN 'C'
+        WHEN COALESCE(ui_change, 'no') = 'yes' AND COALESCE(desc_change, 'yes') = 'yes' THEN 'D'
+        ELSE 'Unknown'  -- In case there's an unexpected combination
+    END AS group_name,
+    SUM(i.amount) AS total_sales
+FROM test_analysis t
+JOIN invoices i ON t.userid = i.userid
+GROUP BY group_name;
+
+-- Query name: monthly_sales_query
+SELECT amount, datepaid
+FROM invoices;
